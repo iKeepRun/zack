3d4767d77ef54124bf6dbd93a4edb034b6bcc96e
czq <592012072@qq.com>
2024-12-03 16:54:45 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles/border.css";
+
+document.documentElement.style.fontSize=document.documentElement.clientWidth/375*100+"px";
+window.addEventListener('resize',()=>{
+  document.documentElement.style.fontSize=document.documentElement.clientWidth/375*100+"px";
+
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
