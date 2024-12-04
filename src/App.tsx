3936108a5c50b0a36bfc2235fb118a9e094a02dd
czq <592012072@qq.com>
2024-12-04 10:15:57 +0800

    lesson6-3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-
+import "./styles/app.css"
+import {  useEffect, useRef } from "react";
 function App() {
-
+  const ref=useRef<HTMLDivElement>(null!);
+  useEffect(()=>{
+    ref.current.style.opacity="1";
+  },[])
   return (
-    <div className='border-bottom' style={{fontSize:'.2rem'}}>
-      hello world!hello world!hello world
+
+    <div ref={ref} className="page guide-page">
+       <img alt="欢乐购" className="main-pic" src ={require('./img/logo_@2x.png')} />
+       <p className="title">欢乐购</p>
+       <img alt="欢乐购" className="sub-pic" src ={require('./img/slogn_word_icon_@2x.png')} />
+       <div className="iconfont arrow-icon">&#xe60c;</div>
     </div>
   );
 }
